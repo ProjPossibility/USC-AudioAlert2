be8e59390b45a60a6dcb5b4aa851f3046285e193
jjw1707 <jjw1707@gmail.com>
2013-02-02 15:47:07 -0800

    sound recording and playback

--- AudioAlert/AudioAlertViewController.h
+++ AudioAlert/AudioAlertViewController.h
@@ -7,7 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreAudio/CoreAudioTypes.h>
 
-@interface AudioAlertViewController : UIViewController
+@interface AudioAlertViewController : UIViewController <AVAudioRecorderDelegate> {
+    IBOutlet UIButton *playButton;
+    IBOutlet UIButton *recButton;
+    IBOutlet UILabel *recStateLabel;
+    
+    BOOL isRecording;
+    BOOL isPlaying;
+    
+    NSURL *temperoryRecFile;
+    AVAudioRecorder *recorder;
+    AVAudioPlayer *player;
+}
+
+@property(nonatomic, retain) IBOutlet UIButton *playButton;
+@property(nonatomic, retain) IBOutlet UIButton *recButton;
+
+-(IBAction)recording;
+-(IBAction)playBack;
 
 @end
--- AudioAlert/AudioAlertViewController.m
+++ AudioAlert/AudioAlertViewController.m
@@ -7,23 +7,83 @@
 //
 
 #import "AudioAlertViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface AudioAlertViewController ()
 
 @end
 
 @implementation AudioAlertViewController
+@synthesize recButton, playButton;
 
 - (void)viewDidLoad
 {
+    isRecording = NO;
+    isPlaying = NO;
+    playButton.hidden = YES;
+    recStateLabel.text = @"Not Recording";
+    
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    
+    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    [audioSession setActive:YES error:nil];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
+    //viewDidUnload
+    NSFileManager *fileHandler = [NSFileManager defaultManager];
+    [fileHandler removeItemAtPath:temperoryRecFile error:nil];
+    recorder = nil;
+    temperoryRecFile = nil;
+    playButton.hidden = YES;
+    
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)recording {
+    if (isRecording == NO) {
+        isRecording = YES;
+        [recButton setTitle:@"Stop" forState:UIControlStateNormal];
+        playButton.hidden = YES;
+        recStateLabel.text = @"Recording...";
+        
+        temperoryRecFile = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:@"soundFile"]];
+        
+        recorder = [[AVAudioRecorder alloc] initWithURL:temperoryRecFile settings:nil error:nil];
+        
+        [recorder setDelegate:self];
+        [recorder prepareToRecord];
+        [recorder record];
+        
+    } else {
+        isRecording = NO;
+        [recButton setTitle:@"REC" forState:UIControlStateNormal];
+        playButton.hidden = NO;
+        recStateLabel.text = @"Not Recording";
+        
+        [recorder stop];
+        
+    }
+}
+- (IBAction)playBack {
+    AVAudioPlayer *player = [[AVAudioPlayer alloc] initWithContentsOfURL:temperoryRecFile error:nil];
+    
+    player.volume = 1;
+    
+    [player play];
+    
+    while (player.playing == YES) {
+        recStateLabel.text = @"Playing...";
+        playButton.enabled = NO;
+    }
+    
+    recStateLabel.text = @"Not Recording";
+    playButton.enabled = YES;
 }
 
 @end
